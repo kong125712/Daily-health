cc81bab2ceb92c7c0900ce8d4d81b32e5616237f
Dracen <124852216+Dracen-W@users.noreply.github.com>
2026-07-14 17:53:21 +0800

    Update MainActivity.java

--- android/app/src/main/java/com/dailyhealth/mobile/MainActivity.java
+++ android/app/src/main/java/com/dailyhealth/mobile/MainActivity.java
@@ -1,18 +1,3 @@
 package com.dailyhealth.mobile;
 
-import android.os.Bundle;
-import android.webkit.WebView;
-
 import com.getcapacitor.BridgeActivity;
-
-public class MainActivity extends BridgeActivity {
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-
-        // 仅在 debug 构建中启用 WebView 远程调试
-        if (BuildConfig.DEBUG) {
-            WebView.setWebContentsDebuggingEnabled(true);
-        }
-    }
-}
