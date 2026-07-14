f033ec2f4ff2d15eaf7b2577672695e1583efafb
Dracen <124852216+Dracen-W@users.noreply.github.com>
2026-07-14 17:38:30 +0800

    Update MainActivity.java

--- android/app/src/main/java/com/dailyhealth/mobile/MainActivity.java
+++ android/app/src/main/java/com/dailyhealth/mobile/MainActivity.java
@@ -3,3 +3,7 @@ package com.dailyhealth.mobile;
 import com.getcapacitor.BridgeActivity;
 
 public class MainActivity extends BridgeActivity {}
+
+if (0 != (getApplicationInfo().flags & ApplicationInfo.FLAG_DEBUGGABLE)) {
+    WebView.setWebContentsDebuggingEnabled(true);
+}
