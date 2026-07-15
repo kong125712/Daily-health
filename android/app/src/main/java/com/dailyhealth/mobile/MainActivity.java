313fd7b0ed5edab389ea8a2ba6a0568a5b5e8f5f
kong125712 <298131802+kong125712@users.noreply.github.com>
2026-07-15 18:49:27 +0800

    Ensure Android SQLite plugin is registered

--- android/app/src/main/java/com/dailyhealth/mobile/MainActivity.java
+++ android/app/src/main/java/com/dailyhealth/mobile/MainActivity.java
@@ -1,5 +1,14 @@
 package com.dailyhealth.mobile;
 
+import android.os.Bundle;
 import com.getcapacitor.BridgeActivity;
+import com.getcapacitor.community.database.sqlite.CapacitorSQLitePlugin;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        // Keep the device database available even when a stale generated plugin manifest is present.
+        registerPlugin(CapacitorSQLitePlugin.class);
+        super.onCreate(savedInstanceState);
+    }
+}
